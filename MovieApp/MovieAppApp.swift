dfd55dc861e76b76491ee823547dd641c0a9e87d
Alper Kavusturan <bilgi@alperkavusturan.com>
2021-01-14 16:57:09 +0100

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Alper Kavusturan on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
